0c6db37e99f074095c2bc7a87200f20d739ac42e
KeerthiKongari <keerthikongari26@gmail.com>
2020-06-30 23:03:59 +0530

    stage two completed

--- chatbot.py
+++ chatbot.py
@@ -1,2 +1,7 @@
-print("""Hello! My name is keerthi.
-I was created in 2020.""")
+print('Hello! My name is Aid.')
+print('I was created in 2020.')
+print('Please, remind me your name.')
+
+# reading a name
+your_name = input()
+print('What a great name you have, {0}!'.format(your_name))
