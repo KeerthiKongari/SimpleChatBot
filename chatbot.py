520d0816515fcbe138808ba249a84ae94c7f6fa5
KeerthiKongari <keerthikongari26@gmail.com>
2020-06-30 23:06:30 +0530

    stage three done

--- chatbot.py
+++ chatbot.py
@@ -2,6 +2,16 @@ print('Hello! My name is Aid.')
 print('I was created in 2020.')
 print('Please, remind me your name.')
 
-# reading a name
-your_name = input()
-print('What a great name you have, {0}!'.format(your_name))
+name = input()
+
+print('What a great name you have, ' + name + '!')
+print('Let me guess your age.')
+print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+# reading all remainders
+remainder_3 = int(input()) % 3
+remainder_5 = int(input()) % 5
+remainder_7 = int(input()) % 7
+your_age = (remainder_3 * 70 + remainder_5 * 21 + remainder_7 * 15) % 105
+
+print(f"Your age is {your_age}; that's a good time to start programming!")
